7d68e201989b634366018838bc594510877fa494
michael <michaelwdysart@gmail.com>
2020-02-29 00:13:21 -0500

    Added answer to survey question.

--- src/main/java/survey/OpenEndedQuestion.java
+++ src/main/java/survey/OpenEndedQuestion.java
@@ -1,9 +1,24 @@
 package survey;
 
+import java.util.ArrayList;
+
 public class OpenEndedQuestion extends Question {
+
+    private ArrayList<String> answers = new ArrayList<String>();
 
     public OpenEndedQuestion(String question) {
         super("open_ended", question);
     }
 
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
+    public void addAnswer(String answer) {
+        this.answers.add(answer);
+    }
 }
