83c78cd32fd88937a8dcd2e31fbbafe7709a0d31
jacksonschoe <jacksonschoenermarck@cmail.carleton.ca>
2020-02-29 22:06:13 -0500

    Added retrieve and get survey

--- src/main/java/webApp/webController.java
+++ src/main/java/webApp/webController.java
@@ -2,12 +2,25 @@ package webApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import survey.Survey;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Controller
 public class webController {
     @Autowired
-    private Object repo;
+    private SurveyRepository repo;
+
+    @GetMapping("/index")
+    public String index(Model model){
+        return "index";
+    }
 
     @PostMapping("/createSurvey")
     public String createSurvey() {
@@ -15,12 +28,20 @@ public class webController {
     }
 
     @PostMapping("/retrieveSurvey")
-    public String retrieveSurvey() {
-        return "";
+    @ResponseBody
+    public String retrieveSurvey(@RequestParam (name="name") String name, Model model) {
+         Survey survey = repo.findByName(name);
+
+         model.addAttribute("survey", survey)
+
+        return "retrieveSurvey";
     }
 
-    @PostMapping("/getSurveys")
-    public String getSurveys() {
-        return "";
+    @GetMapping("/getSurveys")
+    public String getSurveys(Model model) {
+        ArrayList<Survey> surveys = repo.findAll().collect(Collectors.toList());
+
+        model.addAttribute("surveys", surveys);
+        return "getSurvey";
     }
 }
