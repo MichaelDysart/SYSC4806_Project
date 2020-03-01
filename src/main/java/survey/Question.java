add8c3bff3cd9073475e519a2789d7d3a9efd82c
michael <michaelwdysart@gmail.com>
2020-02-29 20:58:47 -0500

    Updated javadoc

--- src/main/java/survey/Question.java
+++ src/main/java/survey/Question.java
@@ -10,35 +10,64 @@ public abstract class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    protected Integer id;
+    protected long id;
     protected String type;
     protected String question;
 
+    /*
+     * A constructor
+     * @param name {String}
+     * @param question {String}
+     */
     public Question(String type, String question) {
         this.type = type;
         this.question = question;
     }
 
-    public Integer getId() {
+    /*
+     * Retrieve the id
+     * @returns {long}
+     */
+    public long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    /*
+     * Sets the id
+     * @param id {long}
+     */
+    public void setId(long id) {
         this.id = id;
     }
 
+    /*
+     * Retrieve the type of the question
+     * @returns {String}
+     */
     public String getType() {
         return type;
     }
 
+    /*
+     * Sets the type
+     * @param type {String}
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /*
+     * Retrieve the question
+     * @returns {String}
+     */
     public String getQuestion() {
         return question;
     }
 
+    /*
+     * Sets the question
+     * @param question {String}
+     */
     public void setQuestion(String question) {
         this.question = question;
     }
--- src/main/java/survey/Survey.java
+++ src/main/java/survey/Survey.java
@@ -7,40 +7,73 @@ import java.util.Collection;
 public class Survey {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    private Integer id;
+    private long id;
 
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL)
     private Collection<Question> questions;
 
+    /*
+     * A constructor
+     * @param name {String}
+     */
     public Survey(String name){ this.name = name; }
 
+    /*
+     * A constructor
+     * @param name {String}
+     * @param questions {Collection<Question>}
+     */
     public Survey(String name, Collection<Question> questions){
         this.name = name;
         this.questions = questions;
     }
 
+    /*
+     * Sets the name
+     * @param name {String}
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /*
+     * Retrieve the name
+     * @returns {String}
+     */
     public String getName() {
         return this.name;
     }
 
-    public void setId(Integer id) {
+    /*
+     * Sets the id
+     * @param id {long}
+     */
+    public void setId(long id) {
         this.id = id;
     }
 
-    public Integer getId() {
+    /*
+     * Retrieve the id
+     * @returns {long}
+     */
+    public long getId() {
         return this.id;
     }
 
+    /*
+     * Sets the list of questions
+     * @param questions {Collection<Question>}
+     */
     public void setQuestions(Collection<Question> questions) {
         this.questions = questions;
     }
 
+    /*
+     * Retrieve the list of questions
+     * @returns {Collection<Question>}
+     */
     public Collection<Question> getQuestions() {
         return questions;
     }
