fee6e99d67532dc0fd59ac470b6558496c5c40e7
michael <michaelwdysart@gmail.com>
2020-02-29 19:56:53 -0500

    Removed unnecessary imports

--- src/main/java/repository/QuestionRepository.java
+++ src/main/java/repository/QuestionRepository.java
@@ -1,10 +1,8 @@
 package repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import survey.Question;
-import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "question", path = "question")
 public interface QuestionRepository extends PagingAndSortingRepository<Question, Long> {
