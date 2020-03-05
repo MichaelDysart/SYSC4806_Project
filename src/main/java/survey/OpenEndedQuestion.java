726e618e13469e694572b66b3d325b59fc3784a6
michael <michaelwdysart@gmail.com>
2020-03-04 22:14:20 -0500

    Removed unneccessary imports

--- src/main/java/survey/OpenEndedQuestion.java
+++ src/main/java/survey/OpenEndedQuestion.java
@@ -1,9 +1,6 @@
 package survey;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.ArrayList;
 
 /*
