a4aa2a07206723590f9c32a0f2872062ddbefad0
michael <michaelwdysart@gmail.com>
2020-03-19 23:44:48 -0400

    Added tests for SurveyIDList

--- src/test/java/survey/OpenEndedQuestionTest.java
+++ src/test/java/survey/OpenEndedQuestionTest.java
@@ -1,3 +1,5 @@
+package survey;
+
 import org.junit.Before;
 import org.junit.Test;
 import survey.OpenEndedQuestion;
@@ -14,7 +16,7 @@ public class OpenEndedQuestionTest {
     OpenEndedQuestion oeq4;
 
     @Before
-    public void setup() throws Exception{
+    public void setup() {
         oeq1 = new OpenEndedQuestion();
         oeq2 = new OpenEndedQuestion("Are you cool?");
         oeq3 = new OpenEndedQuestion("On a scale from 1-10, how much do you enjoy question quail?");
@@ -23,16 +25,16 @@ public class OpenEndedQuestionTest {
 
     @Test
     public void getSetAddAnswers(){
-        ArrayList<String> a1 = new ArrayList<String>(Arrays.asList("yes","no","maybe"));
-        ArrayList<String> a2 = new ArrayList<String>(Arrays.asList("9001","9000"));
+        ArrayList<String> a1 = new ArrayList<>(Arrays.asList("yes","no","maybe"));
+        ArrayList<String> a2 = new ArrayList<>(Arrays.asList("9001","9000"));
 
         oeq2.setAnswers(a1);
         oeq4.setAnswers(a2);
-        assertEquals(oeq2.getAnswers(),new ArrayList<String>(Arrays.asList("yes","no","maybe")));
-        assertEquals(oeq4.getAnswers(),new ArrayList<String>(Arrays.asList("9001","9000")));
+        assertEquals(oeq2.getAnswers(),new ArrayList<>(Arrays.asList("yes","no","maybe")));
+        assertEquals(oeq4.getAnswers(),new ArrayList<>(Arrays.asList("9001","9000")));
 
         oeq2.addAnswer("so");
-        assertEquals(oeq2.getAnswers(),new ArrayList<String>(Arrays.asList("yes","no","maybe","so")));
+        assertEquals(oeq2.getAnswers(),new ArrayList<>(Arrays.asList("yes","no","maybe","so")));
     }
 
     @Test
